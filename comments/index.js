1826f7db6ce76bc799c2bcfeabe9c4935c0e2bed
Makmakkerti <makmakkerti007@gmail.com>
2022-10-19 11:52:16 +0300

    Posts and comments basic setup

---
+++ comments/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+	res.json(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+	const id = req.params.id;
+	const commentId = randomBytes(4).toString('hex');
+	const { content } = req.body;
+	const newComment = { id: commentId, content };
+	commentsByPostId[id] = commentsByPostId[id]
+		? [...commentsByPostId[id], newComment]
+		: [newComment];
+
+	res.status(201).send(commentsByPostId[id]);
+});
+
+app.listen(4001, () => {
+	console.log('[Comments app]: Listening on 4001');
+});
